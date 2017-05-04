56e1cb16f02a13d168c499ee227ac4ba951e12d1
GuyMaor <GuyAvrMaor@gmail.com>
2017-05-03 21:02:59 -0500

    I edited the code so there isn't an error in the angle at the time of take off.

--- QuadcopterCode/QuadcopterCode.ino
+++ QuadcopterCode/QuadcopterCode.ino
@@ -38,16 +38,18 @@ int distance; // Sonar
 int moveNumber; //Determines how severly pitching, yawing, or rolling effects quadcopter.
 
 //IMU(accelerometer stuff)
-int gyro_x, gyro_y, gyro_z;
+double gyro_x, gyro_y, gyro_z;
 long acc_x, acc_y, acc_z, acc_total_vector;
 int temperature;
-long gyro_x_cal, gyro_y_cal, gyro_z_cal;
+double gyro_x_cal, gyro_y_cal, gyro_z_cal;
 long loop_timer;
 float angle_pitch, angle_roll;
 int angle_pitch_buffer, angle_roll_buffer;
 boolean set_gyro_angles;
 float angle_roll_acc, angle_pitch_acc;
 float angle_pitch_output, angle_roll_output;
+float angle_pitch_output_cal, angle_roll_output_cal;
+
 
 /*
  * General Structure for PID controller 
@@ -85,12 +87,12 @@ void setup() {
   //Initialize PID's here
 
   //Initialize PID coefficients
-  pitchPID.integralCoefficient = 0.001;
-  pitchPID.derivativeCoefficient = 0.2;
-  pitchPID.proportionalCoefficient = 0.1;
-  rollPID.integralCoefficient = 0.001;
-  rollPID.derivativeCoefficient = 0.2;
-  rollPID.proportionalCoefficient = 0.1;
+  pitchPID.integralCoefficient = 0;
+  pitchPID.derivativeCoefficient = 0.4;//0.2
+  pitchPID.proportionalCoefficient = 1;
+  rollPID.integralCoefficient = 0;//0.001
+  rollPID.derivativeCoefficient = 0.4;//0.2;
+  rollPID.proportionalCoefficient = 1;
   //Initialize PID other values:
   pitchPID.target = 0; //Need to decide if this value should be transformed into an angle
   pitchPID.timeNow = millis();
@@ -104,16 +106,18 @@ void setup() {
   
   setup_mpu_6050_registers();                                          //Setup the registers of the MPU-6050 (500dfs / +/-8g) and start the gyro
 
-  for (int cal_int = 0; cal_int < 1000 ; cal_int ++){                  //Run this code 2000 times
+  int itterations = 500;
+  for (int cal_int = 0; cal_int < itterations ; cal_int ++){                  //Run this code 2000 times
     read_mpu_6050_data();                                              //Read the raw acc and gyro data from the MPU-6050
     gyro_x_cal += gyro_x;                                              //Add the gyro x-axis offset to the gyro_x_cal variable
     gyro_y_cal += gyro_y;                                              //Add the gyro y-axis offset to the gyro_y_cal variable
-    gyro_z_cal += gyro_z;                                              //Add the gyro z-axis offset to the gyro_z_cal variable
-    delay(3);                                                          
+    gyro_z_cal += gyro_z;                                              //Add the gyro z-axis offset to the gyro_z_cal variable    
+    delay(3);
+    Serial.println(itterations - cal_int);                                                          
   }
-  gyro_x_cal /= 1000;                                                  //Divide the gyro_x_cal variable by 2000 to get the avarage offset
-  gyro_y_cal /= 1000;                                                  //Divide the gyro_y_cal variable by 2000 to get the avarage offset
-  gyro_z_cal /= 1000;                                                  //Divide the gyro_z_cal variable by 2000 to get the avarage offset
+    gyro_x_cal /= itterations;
+    gyro_y_cal /= itterations;
+    gyro_z_cal /= itterations;
 
   escFrontLeft.attach(5); // Callibrate first ESC
   escFrontRight.attach(10); // Callibrate second ESC 
@@ -141,11 +145,14 @@ void setup() {
 double bias1 = 2.0;
 double bias2 = 2.0;
 void loop() {
-  throtle = escData(recevierReadingChecker(pulseIn(4, HIGH, 25000))); // Get information from remote controller
+
   pitch = angle(recevierReadingChecker(pulseIn(8,HIGH,25000)));
   yaw = angle(recevierReadingChecker(pulseIn(12,HIGH, 25000)))/20*(-1);
   roll = angle(recevierReadingChecker(pulseIn(7,HIGH, 25000)));
+  throtle = escData(recevierReadingChecker(pulseIn(4, HIGH, 25000)));// Get information from remote controller  
   
+//  Serial.print("throtle: ");
+//  Serial.println(pulseIn(4, HIGH, 25000));
   //Get data from sonar and accellerometer
   //distance = ultrasonic.Ranging(1);//Reads distance in CM
   
@@ -165,44 +172,57 @@ void loop() {
 //  }
   //Write throttle values
   if(throtle > 1){ 
-    pitchAdjustment = calculatePID(pitchPID, angle_pitch_output-1.0, pitch);
-    rollAdjustment = calculatePID(rollPID, angle_roll_output+4.0, roll);
-    Serial.println("Pitch Adjustment: ");
-    Serial.println(pitchAdjustment);
-    Serial.println("Roll Adjustment: ");
-    Serial.println(rollAdjustment);
+    //                             (PID     ,accellerometer val,  target val)
+    //pitchAdjustment = calculatePID(pitchPID, angle_pitch_output-1.0, pitch);
+    //rollAdjustment = calculatePID(rollPID, angle_roll_output+4.0, roll);
+    pitchAdjustment = calculatePID(pitchPID, angle_pitch_output-angle_pitch_output_cal, pitch);
+    rollAdjustment = calculatePID(rollPID, angle_roll_output-angle_roll_output_cal, roll);
+    Serial.print("Pitch Adjustment: ");
+    Serial.print(pitchAdjustment);
+    Serial.print("Roll Adjustment: ");
+    Serial.print(rollAdjustment);
     escFrontLeft.write(safety((double)(90 + throtle + pitchAdjustment + rollAdjustment + yaw))); //+PIDPitch +PIDRoll
     escFrontRight.write(safety((double)(90 + throtle + pitchAdjustment - rollAdjustment - yaw+bias1)));
     escBackLeft.write(safety((double)(90 + throtle - pitchAdjustment + rollAdjustment - yaw)));
     escBackRight.write(safety((double)(90 + throtle - pitchAdjustment - rollAdjustment + yaw+bias2)));
-    Serial.print("escFrontLeft: ");
-    Serial.println(safety((double)(90 + throtle + pitchAdjustment + rollAdjustment + yaw))); 
-    Serial.print("escFrontRight: ");
-    Serial.println(safety((double)(90 + throtle + pitchAdjustment - rollAdjustment - yaw+bias1))); 
-    Serial.print("escBackLeft: ");
-    Serial.println(safety((double)(90 + throtle - pitchAdjustment + rollAdjustment - yaw))); 
-    Serial.print("escBackRight: ");
-    Serial.println(safety((double)(90 + throtle - pitchAdjustment - rollAdjustment + yaw+bias2))); 
+//    Serial.print("escFrontLeft: ");
+//    Serial.print(safety((double)(90 + throtle + pitchAdjustment + rollAdjustment + yaw))); 
+//    Serial.print("escFrontRight: ");
+//    Serial.print(safety((double)(90 + throtle + pitchAdjustment - rollAdjustment - yaw+bias1))); 
+//    Serial.print("escBackLeft: ");
+//    Serial.print(safety((double)(90 + throtle - pitchAdjustment + rollAdjustment - yaw))); 
+//    Serial.print("escBackRight: ");
+//    Serial.print(safety((double)(90 + throtle - pitchAdjustment - rollAdjustment + yaw+bias2))); 
   }
   else{
+    Serial.print("Pitch: ");
+    Serial.print(angle_pitch);
+    Serial.print("Roll: ");
+    Serial.print(angle_roll);
+    angle_pitch_output_cal = angle_pitch_output;
+    angle_roll_output_cal = angle_roll_output;
+//    angle_pitch = 0;       DON-T USE                         
+//    angle_roll = 0;
+    read_mpu_6050_data();                                              //Read the raw acc and gyro data from the MPU-6050
     escFrontLeft.write(safety((double)(90 + throtle))); //+PIDPitch +PIDRoll
     escFrontRight.write(safety((double)(90 + throtle+bias1)));
     escBackLeft.write(safety((double)(90 + throtle)));
     escBackRight.write(safety((double)(90 + throtle+bias2)));
-    Serial.print("escFrontLeft: ");
-    Serial.println(safety((double)(90 + throtle))); 
-    Serial.print("escFrontRight: ");
-    Serial.println(safety((double)(90 + throtle+bias1))); 
-    Serial.print("escBackLeft: ");
-    Serial.println(safety((double)(90 + throtle))); 
-    Serial.print("escBackRight: ");
-    Serial.println(safety((double)(90 + throtle+bias2))); 
+//    Serial.print("escFrontLeft: ");
+//    Serial.print(safety((double)(90 + throtle))); 
+//    Serial.print("escFrontRight: ");
+//    Serial.print(safety((double)(90 + throtle+bias1))); 
+//    Serial.print("escBackLeft: ");
+//    Serial.print(safety((double)(90 + throtle))); 
+//    Serial.print("escBackRight: ");
+//    Serial.print(safety((double)(90 + throtle+bias2))); 
   }
 //  // Print troubleshooting data
 
 // Print troubleshooting data.
-  Serial.print("Pitch: " ); Serial.print(angle_pitch_output-1.0);
-  Serial.print("| Roll: "); Serial.println(angle_roll_output+4.0);
+//  Serial.print("Pitch: " ); Serial.print(angle_pitch_output-1.0);
+//  Serial.print("| Roll: "); Serial.println(angle_roll_output+4.0);
+  Serial.println();
 
 }
 
@@ -231,8 +251,8 @@ void shutdown(){
 }
 
 float safety(float data){
-  if(data > 105){
-    return 105;
+  if(data > 115){
+    return 115;
   }
   else if(data < 90){
     return 90;
@@ -244,6 +264,7 @@ float safety(float data){
 }
 
 float recevierReadingChecker(float x){ // Clips Reviecer input to a certain range.
+
   if(x < 1070){
     return 1070;
   }
